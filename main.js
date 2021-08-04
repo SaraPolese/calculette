04458618849add76535acf1046a46b7c9ee5ab8e
SARA <sara.polese.9092@gmail.com>
2021-08-04 11:16:05 +0200

    function sustraction

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function gosus (){
+    let num1= document.getElementById("numero1").value;
+    let num2= document.getElementById("numero2").value;
+    let sus= parseInt(num1) - parseInt(num2)
+    
+    document.getElementById("goresult").value = sus;
+  
+  }
+  
